ce8219b400d26cfe7f7679c953b6d93dd68bdbfa
James Chen <james@jobox.ai>
2023-05-06 13:57:18 -0700

    add load calculator

---
+++ loan_calculator.py
@@ -0,0 +1,28 @@
+# Get details of loan
+
+money_owed = float(input("how much do you owe, in dollars?\n")) # $50,000
+apr = float(input("what is the annual percentage rate of the loan?\n")) # 3%
+payment = float(input("how much will you pay off each month, in dollars?\n")) # $1,000
+months = int(input("how many months do you want to see the results for?\n")) # 24
+
+# calculate monthly rate
+monthly_rate = apr/100/12
+
+for i in range(months):
+    # calucate interest to pay
+    interest_paid = money_owed*monthly_rate
+
+    # add in interest
+    money_owed = money_owed + interest_paid
+
+    if (money_owed - payment < 0):
+        print("The last payment is", round_money_owed)
+        print('You paid off the loan in', i+1, 'months')
+        break
+
+    # make payment
+    money_owed = money_owed - payment
+    round_money_owed = round(money_owed, 2) # round dollar to two decimal place
+
+    print('Paid', payment, 'of which', interest_paid, 'was interest', end=' ')
+    print('Now I owe', round_money_owed)
